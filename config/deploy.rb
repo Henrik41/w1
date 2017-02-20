102086f76d9d9dd6fb1997f4224e770e3614d975
Henri Knafo <cmoi40@live.ca>
2017-02-19 21:08:31 -0500

    Set up mysql2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-
-# Change these
 server '45.79.135.198', port: 22, roles: [:web, :app, :db], primary: true
 
 
@@ -9,25 +7,18 @@ set :user,            'bob'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-# setup rvm.
-set :rbenv_path, '/root/.rbenv'
-set :rbenv_type, :system
-set :rbenv_ruby, '2.3.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-
-# Don't change these unless you know what you're doing
+## Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        true
+set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/var/www/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
