ca84e2d10daee79cb0ec396debb08f056a395f95
Henri Knafo <cmoi40@live.ca>
2017-02-26 19:07:16 -0500

    addd config key to application.rb 2

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative 'boot'
 require 'rails/all'
 
 
-config.secret_key_base = 'f7ff5f571ffa7186eeb14006f7c4209643750b7879d360e1a8d0ef6c531284c496c9f72b81a22a5bd279c10495adaabd413c80a8585b8bc385b6492099e16759'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,5 +12,7 @@ module W1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.secret_key_base = 'f7ff5f571ffa7186eeb14006f7c4209643750b7879d360e1a8d0ef6c531284c496c9f72b81a22a5bd279c10495adaabd413c80a8585b8bc385b6492099e16759'
+    
   end
 end
