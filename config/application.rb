6491fc2c2a39c054c809b988e0f7cdb82c1d0cd8
Henri Knafo <cmoi40@live.ca>
2017-02-26 19:03:38 -0500

    addd config key to application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+config.secret_key_base = 'f7ff5f571ffa7186eeb14006f7c4209643750b7879d360e1a8d0ef6c531284c496c9f72b81a22a5bd279c10495adaabd413c80a8585b8bc385b6492099e16759'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
