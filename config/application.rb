b549fdba026144097d0ecf0721e4d0a93fe32cdb
Henri Knafo <cmoi40@live.ca>
2017-02-26 21:04:56 -0500

    addd config key to application.rb 11

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module W1
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.secret_key_base =  '<%= ENV["SECRET_KEY_BASE"] %>'
+    
   end
 end
